74661676fd29622f8e8a260e27ddd7e2d78f19ee
tatsu <naz1mb20@gmail.com>
2022-09-05 15:10:10 +0100

    jquery version

---
+++ index_jq.js
@@ -0,0 +1,26 @@
+jQuery('.drum').click(function(){
+  console.log("clicked")
+  playSound(jQuery(this));
+  buttonAnimation(jQuery(this));
+});
+
+jQuery(document).keydown(function(event){
+  jQuery('.drum').each(function(){
+    if (event.key.toLowerCase() == jQuery(this).text().toLowerCase()){
+      playSound(jQuery(this));
+      buttonAnimation(jQuery(this));
+    }
+  })
+})
+
+function playSound(drumObj){
+  var audio = new Audio("sounds/" + drumObj.attr('id') + ".mp3");
+  audio.play();
+}
+
+function buttonAnimation(drumObj){
+  drumObj.addClass("pressed");
+  const myTimeout = setTimeout(function(){
+    drumObj.removeClass("pressed");
+  }, 100);
+}
